27dc6bb0ce6c38e11803ae1c9f248f3133628335
Aarti-Jain <aartijain@brandeis.edu>
2023-02-04 16:12:55 -0500

    home route defined

--- VVA/demo 2/src/main/java/VVA/demo/DemoApplication.java
+++ VVA/demo 2/src/main/java/VVA/demo/DemoApplication.java
@@ -2,12 +2,26 @@ package VVA.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@GetMapping("/home")
+	public String home(){
+		return (String.format("Hello World!"));
+	} 
+
+	//@PostMapping ("/enter")
+	// public void 
+
 
 }
