f8c52da863da0b9f48af396952a4e355374bdbbe
gillianyiwang <gillian.yi.wang@gmail.com>
2023-02-04 15:28:52 -0500

    VVA updated

---
+++ VVA/demo 2/src/test/java/VVA/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package VVA.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
